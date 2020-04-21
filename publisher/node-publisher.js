e262b08177b2feadb6e9f9601210942b63ada38d
reesh-a <Reeshabh.Agrawal@morganstanley.com>
2020-04-21 11:26:32 -0400

    Adding node publisher app for testing

---
+++ publisher/node-publisher.js
@@ -0,0 +1,53 @@
+const readline = require('readline')
+const http = require('http')
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+})
+
+function ask() {
+
+	rl.question('Topic to publish message to: ', topic => {
+
+		rl.question('JSON message to publish: \n', msg => {
+
+			const options = {
+				hostname: 'localhost',
+				port: 3500,
+				path: `/v1.0/publish/${topic}`,
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			};
+
+			const req = http.request(options, (res) => {
+				console.log(`STATUS: ${res.statusCode}`);
+				console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+				res.setEncoding('utf8');
+				res.on('data', (chunk) => {
+					console.log(`BODY: ${chunk}`);
+				});
+				res.on('end', () => {
+					console.log('No more data in response.');
+				});
+			});
+
+			req.on('error', (e) => {
+				console.error(`problem with request: ${e.message}`);
+			});
+
+			req.write(msg)
+			req.end()
+
+			console.log(`Message sent!`)
+
+			ask()
+
+		})
+	})
+}
+
+console.log("Json message publisher. Dapr URL: 'localhost/3500/v1.0/publish/<topic>' \n")
+ask()
