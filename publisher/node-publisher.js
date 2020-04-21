3dc32f91583ada2863dd6c41601461d93e0638f0
reesh-a <Reeshabh.Agrawal@morganstanley.com>
2020-04-21 12:55:40 -0400

    Node publisher - separating cmd line and server

--- publisher/node-publisher.js
+++ publisher/node-publisher.js
@@ -1,7 +1,5 @@
 const readline = require('readline')
 const http = require('http')
-const express = require('express')
-const app = express()
 
 const rl = readline.createInterface({
 	input: process.stdin,
@@ -51,8 +49,5 @@ function ask() {
 	})
 }
 
-app.get('/', (_req, res) => { res.send("Hello from node dapr publisher") })
-app.listen(3000, () => {
-	console.log("Json message publisher. Dapr URL: 'localhost/3500/v1.0/publish/<topic>' \n")
-	ask()
-})
+console.log("Json message publisher. Dapr URL: 'localhost/3500/v1.0/publish/<topic>' \n")
+ask()
